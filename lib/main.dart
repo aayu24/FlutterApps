19036caa3a5907d537851ef2d4ad3de99858efe8
aayu24 <patni170101001@iitg.ac.in>
2020-04-19 12:19:15 +0530

    Before reactive buttons

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,10 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
-        ),
+            title: Center(
+              child: Text('Dicee'),
+            ),
+            backgroundColor: Colors.red),
         body: DicePage(),
       ),
     ),
@@ -18,6 +19,34 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            //resize to fill the entire row space
+            //flex: 2, //twice as wide as the other one
+            child: FlatButton(
+              //want image to appear as button itself
+              //comes with default padding of 16 from left and right
+              child: Image.asset('images/dice1.png'),
+              onPressed: () {
+                //what happens
+                print('left button got pressed');
+              },
+            ),
+          ),
+          Expanded(
+            //resize to fill the entire row space
+            //flex: 1,
+            child: FlatButton(
+              child: Image.asset('images/dice1.png'),
+              onPressed: () {
+                print('right button got pressed.');
+              }, //this is example of a void callback
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
