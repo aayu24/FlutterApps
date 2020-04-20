ecacd5b2e6a688c541424a4fc6546665550d6738
aayu24 <patni170101001@iitg.ac.in>
2020-04-20 15:21:12 +0530

    Added Ball Stateful widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
@@ -14,8 +15,29 @@ class BallPage extends StatelessWidget {
         backgroundColor: Colors.blue.shade900,
         title: Text('Ask Me Anything'),
       ),
-      body: Container(),
+      body: Ball(),
       backgroundColor: Colors.blue,
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: FlatButton(
+          child: Image.asset('images/ball1.png'),
+          onPressed: () {
+            print('I got clicked.');
+          },
+        ),
+      ),
     );
   }
 }
