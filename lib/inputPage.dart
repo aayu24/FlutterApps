110674f59fbc25b65617f1b033fe2619e4554f00
aayu24 <patni170101001@iitg.ac.in>
2020-04-30 23:12:24 +0530

    Refactored Code, extracted a Widget and made it reusable.

--- lib/inputPage.dart
+++ lib/inputPage.dart
@@ -19,72 +19,51 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15),
-                    //to implement rounder border
-                    decoration: BoxDecoration(
-                      color: Color(0xff1d1e33),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
+                  child: ReusableCard(colour: Color(0xff1d1e33)),
                 ),
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15),
-                    //to implement rounder border
-                    decoration: BoxDecoration(
-                      color: Color(0xff1d1e33),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
+                  child: ReusableCard(colour: Color(0xff1d1e33)),
                 ),
               ],
             ),
           ),
           Expanded(
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    margin: EdgeInsets.all(15),
-                    //to implement rounder border
-                    decoration: BoxDecoration(
-                      color: Color(0xff1d1e33),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+            flex: 1,
+            child: ReusableCard(colour: Color(0xff1d1e33)),
           ),
           Expanded(
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15),
-                    //to implement rounder border
-                    decoration: BoxDecoration(
-                      color: Color(0xff1d1e33),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
+                  child: ReusableCard(colour: Color(0xff1d1e33)),
                 ),
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15),
-                    //to implement rounder border
-                    decoration: BoxDecoration(
-                      color: Color(0xff1d1e33),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
+                  child: ReusableCard(colour: Color(0xff1d1e33)),
                 ),
               ],
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+//Custom widget or Reusable widget
+class ReusableCard extends StatelessWidget {
+  //key class not useful for us, as our widget is stateless/static on screen.
+  final Color
+      colour; //final makes this property immutable , i.e can be set only once
+  ReusableCard({@required this.colour}); //as we require colour
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      //to implement rounder border
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
