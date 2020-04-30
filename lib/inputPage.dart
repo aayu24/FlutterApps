4bc744a789c866e1501cbd489a8e2806d6a88236
aayu24 <patni170101001@iitg.ac.in>
2020-04-30 23:30:25 +0530

    Created constants.

--- lib/inputPage.dart
+++ lib/inputPage.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+//these can be a const as we can work out this value at time of compilation
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xff1d1e33);
+const bottomContainerColor = Color(0xffeb1555);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -19,30 +24,36 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: ReusableCard(colour: Color(0xff1d1e33)),
+                  child: ReusableCard(colour: activeCardColor),
                 ),
                 Expanded(
-                  child: ReusableCard(colour: Color(0xff1d1e33)),
+                  child: ReusableCard(colour: activeCardColor),
                 ),
               ],
             ),
           ),
           Expanded(
             flex: 1,
-            child: ReusableCard(colour: Color(0xff1d1e33)),
+            child: ReusableCard(colour: activeCardColor),
           ),
           Expanded(
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: ReusableCard(colour: Color(0xff1d1e33)),
+                  child: ReusableCard(colour: activeCardColor),
                 ),
                 Expanded(
-                  child: ReusableCard(colour: Color(0xff1d1e33)),
+                  child: ReusableCard(colour: activeCardColor),
                 ),
               ],
             ),
           ),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight, //
+          )
         ],
       ),
     );
@@ -50,10 +61,12 @@ class _InputPageState extends State<InputPage> {
 }
 
 //Custom widget or Reusable widget
+//Since this is stateless , it is immutable/can't be changed
 class ReusableCard extends StatelessWidget {
-  //key class not useful for us, as our widget is stateless/static on screen.
-  final Color
-      colour; //final makes this property immutable , i.e can be set only once
+  //key class not useful for us, as our widget is stateless/static on screen.\
+  //final makes this property immutable , i.e can be set only once
+  final Color colour;
+  //the above can't be const as we only build this after compilation , hence can't resolve the Color at compile time
   ReusableCard({@required this.colour}); //as we require colour
 
   @override
